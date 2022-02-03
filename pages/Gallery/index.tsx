81d2f33a21f800015e10d9c53dd3a6169d174d46
lmagruu <magg4ms@gmail.com>
2022-02-03 17:58:09 -0400

    do all imports and create gallery ts

--- pages/Gallery/index.tsx
+++ pages/Gallery/index.tsx
@@ -1,5 +1,12 @@
 import { NextPage } from "next";
+import SubHeading from "../../components/SubHeading/SubHeading";
 import classes from "./Gallery.module.css";
+import imagesGallery from "../../src/gallery";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
 
 const Gallery: NextPage = () => {
   return <div>Gallery</div>;
---
+++ src/gallery.ts
@@ -0,0 +1,13 @@
+import gallery1 from "../public/static/gallery01.png";
+import gallery2 from "../public/static/gallery02.png";
+import gallery3 from "../public/static/gallery03.png";
+import gallery4 from "../public/static/gallery04.png";
+
+const imagesGallery: StaticImageData[] = [
+  gallery1,
+  gallery2,
+  gallery3,
+  gallery4,
+];
+
+export default imagesGallery;
