39ede175bd6496f8b8659bd47b6955c4730a9d6c
lmagruu <magg4ms@gmail.com>
2022-02-03 23:11:25 -0400

    footer overlay component

--- components/Footer/FooterOverlay.tsx
+++ components/Footer/FooterOverlay.tsx
@@ -1,7 +1,13 @@
 import { NextPage } from "next";
+import classes from "./FooterOverlay.module.css";
 
 const FooterOverLay: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className={classes["app__footerOverlay"]}>
+      <div className={classes["app__footerOverlay-black"]} />
+      <div className={`${classes["app__footerOverlay-img"]} app__bg`} />
+    </div>
+  );
 };
 
 export default FooterOverLay;
--- pages/Footer/index.tsx
+++ pages/Footer/index.tsx
@@ -1,8 +1,16 @@
 import { NextPage } from "next";
+import classes from "./Footer.module.css";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
+import FooterOverLay from "../../components/Footer/FooterOverlay";
+import NewsLetter from "../../components/Footer/NewsLetter";
 
 const Footer: NextPage = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={`${classes["app__footer"]} section__padding`}>
+      <FooterOverLay />
+      <NewsLetter />
+    </div>
+  );
 };
 
 export default Footer;
