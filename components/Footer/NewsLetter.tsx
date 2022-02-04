5b138882ed075a1238187d23ac99cd2cecfc8da3
lmagruu <magg4ms@gmail.com>
2022-02-04 00:24:53 -0400

    finish footer section

--- components/Footer/NewsLetter.tsx
+++ components/Footer/NewsLetter.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from "next";
+import classes from "./NewsLetter.module.css";
+import SubHeading from "../SubHeading/SubHeading";
 
 const NewsLetter: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className={classes["app__newsletter"]}>
+      <div className={classes["app__newsletter-heading"]}>
+        <SubHeading subTitle="Newsletter" />
+        <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+        <p className="p__opensans">And never miss latest Updates!</p>
+      </div>
+      <div className={`${classes["app__newsletter-input"]} flex__center`}>
+        <input type="email" placeholder="Enter your email address" />
+        <button type="button" className="custom__button">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default NewsLetter;
--- pages/Footer/index.tsx
+++ pages/Footer/index.tsx
@@ -1,15 +1,54 @@
 import { NextPage } from "next";
+import NextImage from "next/image";
 import classes from "./Footer.module.css";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi";
 import FooterOverLay from "../../components/Footer/FooterOverlay";
 import NewsLetter from "../../components/Footer/NewsLetter";
+import geritch from "../../public/static/gericht.png";
+import spoon from "../../public/static/spoon.png";
 
 const Footer: NextPage = () => {
   return (
-    <div className={`${classes["app__footer"]} section__padding`}>
+    <footer className={`${classes["app__footer"]} section__padding`}>
       <FooterOverLay />
       <NewsLetter />
-    </div>
+
+      <div className={classes["app__footer-links"]}>
+        <div className={classes["app__footer-links_contact"]}>
+          <h1 className={classes["app__footer-headtext"]}>Contact Us</h1>
+          <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+          <p className="p__opensans">+1 212-344-1230</p>
+          <p className="p__opensans">+1 212-555-1230</p>
+        </div>
+        <div className={classes["app__footer-links_logo"]}>
+          <div className={classes["app__footer-img_logo"]}>
+            <NextImage src={geritch} alt="geritch logo image" layout="fill" />
+          </div>
+          <p className="p__opensans">
+            &quot;The best way to find yourself is to lose yourself in the
+            service of others.&quot;
+          </p>
+          <div className={classes["spoon__container"]}>
+            <NextImage src={spoon} alt="spoon logo" />
+          </div>
+          <div className={classes["app__footer-links_icons"]}>
+            <FiFacebook />
+            <FiTwitter />
+            <FiInstagram />
+          </div>
+        </div>
+        <div className={classes["app__footer-links_work"]}>
+          <h1 className={classes["app__footer-headtext"]}>Working Hours</h1>
+          <p className="p__opensans">Monday-Friday:</p>
+          <p className="p__opensans">08:00 am -12:00 am</p>
+          <p className="p__opensans">Saturday-Sunday:</p>
+          <p className="p__opensans">07:00 am -11:00 pm</p>
+        </div>
+      </div>
+      <div className={classes["footer__copy"]}>
+        <p className="p__opensans">&copy; 2021 Gerícht. All Rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
